9c95856a3056da8eb15c45bbed22116dbb6ea19c
Steve Holmes <steve@thought-sauce.com>
2014-06-19 19:42:37 +0800

    Secret page with authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   get 'home/index'
+  get 'home/secret'
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
